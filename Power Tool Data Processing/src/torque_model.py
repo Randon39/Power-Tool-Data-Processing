45493a38cad1f4563d54a028e2e858e545677b95
Lucas Bernardes <82560811+Randon39@users.noreply.github.com>
2025-11-26 16:45:18 -0300

    Delete Power Tool Data Processing directory

--- Power Tool Data Processing/src/torque_model.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# Converts RMS electrical current into mechanical torque using a linear model.
-# The value of kₜ (the electromechanical gain) was obtained through linear regression performed during motor-brake dynamometer tests on power tools.
-# slope = K_T 
-# intercept = B_T 
-
-def electrical_to_mechanical_torque(current, K_T, B_T):
-    return K_T * current - B_T
